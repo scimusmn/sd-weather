9be9f0260d72619cdd9c459e13cefdee5ead471a
bryan kennedy <bkennedy@smm.org>
2014-10-23 16:08:49 -0500

    Add a helper for css classes

--- meteor/client/helpers/handlebars.js
+++ meteor/client/helpers/handlebars.js
@@ -7,5 +7,18 @@ Custom Handlebars helpers.
 /+ ---------------------------------------------------- */
 
 Handlebars.registerHelper('thisYear', function(myArgument){
-  return new Date().getFullYear();
+    return new Date().getFullYear();
+});
+
+/**
+ * Turn titles into CSS classes
+ *
+ * Remove puncuation
+ * Spaces to undescores
+ * Lowercase text
+ */
+Handlebars.registerHelper('lowerSpacesToDashes', function(input) {
+    if (input) {
+        return input.replace(/[\.,'-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, '').replace(/\s+/g, '-').toLowerCase();
+    }
 });
