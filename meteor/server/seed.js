0d2a27c97cec9e7f9881ee280f9c72ea91281503
bryan kennedy <bkennedy@smm.org>
2014-10-24 16:13:26 -0500

    Normalizing order and IDs

--- meteor/server/seed.js
+++ meteor/server/seed.js
@@ -12,24 +12,40 @@ Fill in the app with dummy data if database is empty.
 if (Locations.find().count() === 0) {
 
     Locations.insert({
+        _id: 'san-diego',
+        order: 0,
+        title: 'San Diego',
+        description: 'Galph korsa ozlint arka arul brul qi galph lydran. Pank ma gronk prinquis, ux nalista flim morvit gronk menardis zorl ma brul obrikt twock arul.',
+        latitude: '32.732323',
+        longitude: '-117.147403'
+    });
+    Locations.insert({
+        _id: 'anza-borego',
+        order: 1,
         title: 'Anza Borego',
         description: 'Pank nalista, fli vo zorl athran ma. Erc cree xu qi ozlint su, twock vusp, su quolt yiphras dri morvit gronk fli er. Berot dri qi vo anu, ozlint velar frimba, srung arka dwint morvit athran helk. Galph su korsa cree, er erc berot dri yem velar re zorl rintax. Berot ma nix, ti lamax er tharn srung ti lamax arka re ozlint teng lamax sernag srung nalista berot twock? Sernag gra furng dri quolt galph fli arul, nix morvit gra gronk. Flim athran nalista brul vo erk erc epp, teng ti qi, ozlint relnag berot.',
         latitude: '33.259167',
         longitude: '-116.399167'
     });
     Locations.insert({
+        _id: 'imperial-beach',
+        order: 2,
         title: 'Imperial Beach',
         description: 'Kurnap harle yem irpsa fli cree lydran zorl, delm morvit ma rintax vusp ewayf ik. Obrikt tharn prinquis urfa. Qi er tharn ma yiphras rhull obrikt ux erk delm, obrikt erk. Rhull kurnap vo yiphras jince tharn jince, fli ju frimba gra? Ux irpsa morvit xu vo ik cree ozlint ux fli, ma tharn er, lydran wex. Galph gra cree urfa yem wex su.',
         latitude: '32.578333',
         longitude: '-117.117222'
     });
     Locations.insert({
+        _id: 'julian',
+        order: 3,
         title: 'Julian',
         description: 'Gen sernag quolt kurnap pank anu. Pank menardis vusp nix fli er jince erc kurnap urfa rintax er epp, clum yiphras gen obrikt yem harle, arka pank zorl quolt. La nix vusp athran er lydran. Anu helk prinquis, srung whik irpsa ux la gra berot gra irpsa berot urfa. Ik gronk harle nix ozlint dwint urfa berot nix ma dri, fli wynlarce su, delm dwint. Rhull ozlint, ti nalista athran anu, harle ik twock, zeuhl vusp nalista, kurnap er. Twock su wex ewayf wex. Flim furng irpsa rhull velar erk thung nix vusp gra pank whik er irpsa velar sernag gronk.',
         latitude: '33.070833',
         longitude: '-116.585556'
     });
     Locations.insert({
+        _id: 'palomar-mountain',
+        order: 4,
         title: 'Palomar Mountain',
         description: 'Vusp morvit xi rintax arka velar gronk ux gen twock dwint ti. Ik morvit la brul nalista nix erc ewayf tharn. Arka brul, twock galph xi urfa galph erk, tharn fli er clum anu athran twock sernag? Er vusp dri quolt rhull harle la prinquis, sernag yiphras, xi twock arul delm ti. Kurnap whik ma, jince teng, ju ik, su lamax yem vo. Quolt, wex dri nalista, zeuhl harle kurnap.',
         latitude: '33.363484',
