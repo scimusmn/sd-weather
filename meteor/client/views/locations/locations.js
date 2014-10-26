07be26d89780d5e761bcbfb02d46cc5958cee9f1
bryan kennedy <bkennedy@smm.org>
2014-10-26 12:23:34 -0500

    Updated template code to show weather inside Locs
    This allows us to use the spacebars templating to do the client side
    joins for us. This way we don't need to write a bunch of JS to control
    the Weather and Location objects. We can do it directly with the {{> }}
    partials.

--- meteor/client/views/locations/locations.js
+++ meteor/client/views/locations/locations.js
@@ -13,39 +13,21 @@ Template.locations.helpers({
 });
 
 Template.locations.rendered = function () {
-
-    var locationsCursor = Locations.find();
-    var locations = locationsCursor.fetch();
-
-    _.each(locations, function (location) {
-        console.log('location - ', location);
-        var latitude = location.latitude;
-        var longitude = location.longitude;
-        var locationClass = lowerSpacesToDashes(location.title);
-        Meteor.call('checkForecast', latitude, longitude, function(error, results) {
-            var temp = results.currently.temperature;
-            console.log('Temp in ' + location.title + ' is ' + temp);
-            $('.' + locationClass + ' h2 .forecast-temp').html(temp);
-        });
-    });
-
     //
-    // Use this once we start writing the data to the DB
-    //
-    //Meteor.call('insertCurrent', data.currently, cityStateZip, lat, lon);
-
-    /**
-     * Dev call to the timer function to test asynchonus calls
-     */
-    Meteor.call('timerTest', function(error, result) {
-        console.log('Client timerTest results - ', result);
-    });
-
 };
 
 Template.locations.events({
   //
 });
+
+Template.locWeather.helpers({
+    weather: function() {
+        return Weather.findOne( { class: this._id } );
+    }
+});
+
+Template.locWeather.rendered = function () {
+};
 
 function lowerSpacesToDashes(input) {
     if (input) {
