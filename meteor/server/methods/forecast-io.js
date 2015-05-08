e916203a633c2eee855a6beeea32c1d2cb2dacfc
bryan kennedy <bkennedy@smm.org>
2015-05-08 12:07:28 -0500

    Removing old unused async check methods
    This code isn't in use and represented an old way to do the cron job of
    checking for the weather data.

--- meteor/server/methods/forecast-io.js
+++ meteor/server/methods/forecast-io.js
@@ -95,91 +95,6 @@ SyncedCron.add({
     }
 });
 
-/**
- * Timer async example.
- *
- * I have this in here to demonstrate how the async wrappers work in Meteor.
- *
- * It should probably come out at some point,
- * but for now it is a useful demonstration.
- */
-var timerTestAsync = function(callback) {
-    Meteor.setTimeout(function () {
-        callback(null, 'Waited 5 seconds before reporting back.');
-    }, 5000);
-};
-
-/**
- * Get forecast.io JSON object for a given lat long
- *
- * This request can take some time and we don't want it to block
- * our page load process. So we label it as a Async function. We'll wrap
- * it in a Meteor helper when we call it.
- */
-var checkForecastAsync = function(latitude, longitude, callback) {
-    var apiKey = Meteor.settings.public.forecastAPI;
-    var protocol = 'https://';
-    var apiDomain = 'api.forecast.io';
-    var apiType = 'forecast';
-    var url = protocol +
-        apiDomain + '/' +
-        apiType + '/' +
-        apiKey + '/' +
-        latitude + ',' +
-        longitude;
-    var forecastResult = HTTP.get(url);
-    //
-    // Execute a callback to return the results
-    //
-    // The first parameter is an error.
-    // The 2nd is the result of the call.
-    //
-    callback(null, forecastResult);
-};
-
-Meteor.methods({
-    /**
-     * Timer async method example
-     */
-    timerTest: function () {
-        // Don't block, or pause, everything while we execute this call.
-        this.unblock();
-
-        var timerTestSync = Meteor.wrapAsync(timerTestAsync);
-        var result;
-        try {
-            result = timerTestSync();
-            console.log('Server timerTest - ', result);
-        }
-        catch(e) {
-            console.log('getData method error' + e);
-        }
-        finally{
-            return result;
-        }
-    },
-    checkForecast: function (latitude, longitude) {
-        // Don't block, or pause, everything while we execute this call.
-        this.unblock();
-
-        var checkForecastSync = Meteor.wrapAsync(checkForecastAsync);
-        var result;
-        try {
-            result = checkForecastSync(latitude, longitude);
-            //
-            // Print the forecast.io JSON result for debugging
-            //
-            //console.log('Server checkForecast - ', result);
-        }
-        catch(e) {
-            console.log('checkForecast error' + e);
-        }
-        finally {
-            return result.data;
-        }
-    }
-});
-
 var checkForecastCron = function(latitude, longitude) {
     var apiKey = Meteor.settings.public.forecastAPI;
     var protocol = 'https://';
@@ -195,4 +110,3 @@ var checkForecastCron = function(latitude, longitude) {
     //console.log('Cron result - forecastResult - ', forecastResult);
     return forecastResult;
 };
-
